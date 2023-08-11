5bf5ec0b76ff439306dbecb39f7277c5b56fc77f
Definite123 <okonobohdefinite2@gmail.com>
2023-08-11 03:07:44 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
+
+ #include <stdio.h>
 
 /**
- * main - this is the main function
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
- * Return: always 0
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
