94923b712b93dcb2a37defd73ef41fdb94e503c8
Definite123 <okonobohdefinite2@gmail.com>
2023-08-17 13:32:32 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - functions that multiplies two intregers
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
